61dd90ec7ab31c2c63c801136186c25816450f23
france-m52 <adrielfranca547@gmail.com>
2025-01-24 16:49:59 -0300

    update 16:49hs 24/01/2025

---
+++ uc1/if1.js
@@ -0,0 +1,68 @@
+let idade = 17
+// Verificando se a idade é válida para votar
+if (idade >= 16) {
+    console.log("Você pode votar!")
+ } else {
+    console.log("Você não pode votar")
+}
+
+// questão 1
+
+let alunoNota1 = 6
+let alunoNota2 = 6
+let media = (alunoNota1 + alunoNota2) / 2
+if (media >= 8) {
+    if (media = 10) {
+        console.log("conceito ótimo")
+    } else (
+        console.log("conceiro bom")
+    )
+} else if (media >= 6){ 
+    console.log("conceito regular")
+}
+else {
+    console.log("conceito insuficiente")
+}
+
+// questão 2
+
+valor1 = 8
+valor2 = 4
+if (valor1 > valor2){ 
+    console.log("o maior valor é "+valor1)
+} else {
+    console.log("o maior valor é "+valor2)
+}
+
+// questão 3
+
+let time = "flamengo"
+if (time == "flamengo"){ 
+    console.log("em dezembro de 81, botou os ingleses na roda")
+} else if (times == "palmeira") { 
+    console.log("não tem mundial")  
+}  else if (times == botafogo){ 
+    console.log("é bairro")
+} else {
+    console.log("time fraco")
+}
+
+
+// questão 4
+ // monte um programa que informe dos valores e informe qual é o maior valor
+ valor3 = 10
+ if (valor1 == valor2){ 
+    console.log("os valores são iguais")
+ } else if ( valor1 == valor3){
+    console.log("os valores são iguais")
+ } else if (valor2 == valor3){
+    console.log("os valores são iguais")
+ } else {
+    if (valor1 > valor2) {
+    console.log("o maior valor é "+valor1)
+    } else if (valor2 > valor1){
+    console.log("o maior valor é "+valor2)
+    } else if (valor2 > valor3) {
+    console.log("o maior valor é "+valor2)
+    }
+}
---
+++ uc1/olaMundo.js
@@ -0,0 +1,59 @@
+
+// questão 1
+
+// nome = "adriel"
+// sobrenome = "franca"
+// console.log(nome +" "+ sobrenome)
+
+// questão 2
+nome = "ppp"
+telefone = "3333"
+email = "fgf"
+nascimento = "2000"
+
+// console.log(`
+// nome: ${nome}
+// telefone: ${telefone}
+// email: ${email}
+// nascimento: ${nascimento}
+// `)
+
+// console.log("nome: " + nome + "\n telefone: " + telefone + "\n email: " + email + "\n nascimento: " + nascimento )
+
+// questão 4 e 5
+
+let valor1 = 12
+valor1+= 4 
+let média = (valor1 + 10) / 2
+console.log(valor1)
+// let valor2 =5
+// let soma = valor1 + valor2
+// let subtração = valor1 - valor2
+// let mult = valor1 * valor2
+// let div = valor1 / valor2
+// let resto = valor1 % valor2
+// console.log("A soma é: "+soma)
+// console.log("a subtração é: "+subtração)
+// console.log("a multiplicação é: "+mult)
+// console.log("a divisão é: "+div)
+// console.log("o resto é: "+resto)
+
+// questão 6
+
+// let altura = 1.81
+// let peso = 87
+// let imc = peso / (altura*altura)
+// console.log("o imc é " +imc)
+
+// questão 7
+
+// let centimetro = 1
+// let metro = centimetro / 100
+// console.log("o valor em metros é " + metro) 
+
+// questão 8
+
+let base = 10 
+let altura = 20 
+let areaRetangulo = (base * altura) / 2
+console.log("a area do retangulo é ") +areaRetangulo
